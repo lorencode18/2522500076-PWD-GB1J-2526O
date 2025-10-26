f41c5b9c8ddb52c4a181774fd69b0f7e02bb85c0
Peter-oxy <2411500082@mahasiswa.atmaluhur.ac.id>
2025-10-26 21:55:24 +0700

    update styling section#contact

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,6 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
-})
+    alert("cion ge");
+});
+let amoy = prompt("siape yang cion");
+console.log("aok ape dak " + amoy + "cion?");
