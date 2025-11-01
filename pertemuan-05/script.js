ff687b597bf485d29a467de575d48ba86dd3b00e
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-01 14:30:30 +0700

    Latihan Ganti Teks Tombol Saat Menu Aktif

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,5 +1,12 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
-  document.querySelector("nav").classList.toggle("active");
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("active");
+
+  if (nav.classList.contains("active")) {
+    this.textContent = "\u2716";
+  } else {
+    this.textContent = "\u2630";
+  }
 });
 
 document.querySelector("form").addEventListener("submit", function (e) {
