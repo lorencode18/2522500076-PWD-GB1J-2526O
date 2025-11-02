590d4804098b82f6a7e9a0622fc8af29333dc2fd
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-02 20:31:33 +0700

    Latihan 4

---
+++ pertemuan-06/lat4.php
@@ -0,0 +1,7 @@
+<?php
+define("DOSEN_PENGAMPU", "Yohanes Setiawan Japriadi, M.Kom.");
+const MATAKULIAH = "Pemrograman Web Dasar";
+
+echo "Dosen_Pengampu: " . DOSEN_PENGAMPU . "<br>";
+echo "Matakuliah: " . MATAKULIAH;
+?>
