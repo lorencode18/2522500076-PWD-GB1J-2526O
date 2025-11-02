6ac91e04dff01d23f7547e0612021b7861a24f9f
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-02 19:59:12 +0700

    Latihan 1

---
+++ pertemuan-06/lat1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Belajar PHP Dasar</title>
+</head>
+<body>
+  <h1><?php echo "Halo, Dunia PHP!"; ?></h1>
+  <h2><?php echo "Selamat datang di PHP, Prita Lauren!"; ?></h2>
+</body>
+</html>
