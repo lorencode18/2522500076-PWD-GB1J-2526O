6a2d530254f03c39d958c354c92b050a3ac60ac6
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-04 09:51:21 +0700

    Latihan konstanta dan komentar/remark

--- pertemuan-06/latihan1-12.php
+++ pertemuan-06/latihan1-12.php
@@ -64,3 +64,20 @@ echo "\nNULL:\n";
 var_dump($nilai_akhir);
 echo "</pre>";
 ?>
+
+<h2>Latihan konstanta dan komentar/remark</h2>
+<?php
+// Membuat konstanta DOSEN_PENGAMPU menggunakan define()
+define("DOSEN_PENGAMPU", "Yohanes Setiawan Japriadi, M.Kom.");
+/*
+    Membuat konstanta MATAKULIAH menggunakan const.
+    Konstanta ini menyimpan nama mata kuliah yang diajar oleh dosen pengampu.
+*/
+const MATAKULIAH = "Pemrograman Web Dasar";
+
+// Menampilkan nama dosen pengampu
+echo "Dosen_Pengampu: " . DOSEN_PENGAMPU . "<br>"; 
+
+// Menampilkan nama mata kuliah
+echo "Matakuliah: " . MATAKULIAH; 
+?> <br><br>
