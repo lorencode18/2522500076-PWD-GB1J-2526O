19b0f3db7d4b08cb473d1f880dd6a4953cf0bc19
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-04 09:57:24 +0700

    Latihan array dan penggunaan print_r() dan var_dump()

--- pertemuan-06/latihan1-12.php
+++ pertemuan-06/latihan1-12.php
@@ -145,3 +145,15 @@ echo "<h3>Daftar Hobi Saya:</h3>";
 for ($i = 0; $i < count($hobi); $i++) {
   echo ($i + 1) . ". " . $hobi[$i] . "<br>";
 }
+
+echo "<hr>";
+echo "<h4>Hasil print_r():</h4>";
+echo "<pre>";
+print_r($hobi);
+echo "</pre>";
+
+echo "<h4>Hasil var_dump():</h4>";
+echo "<pre>";
+var_dump($hobi);
+echo "</pre>";
+?>
