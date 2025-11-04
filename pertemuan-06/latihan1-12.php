8a46512b77b983bb7bd5234027318a3281dec03d
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-04 09:48:31 +0700

    Latihan variabel dan tipe data

--- pertemuan-06/latihan1-12.php
+++ pertemuan-06/latihan1-12.php
@@ -8,3 +8,20 @@
   <h2><?php echo "Selamat datang di PHP, Prita Lauren!"; ?></h2>
 </body>
 </html>
+
+<h2>Latihan variabel dan tipe data</h2>
+<?php
+$nama = "Prita Lauren";
+$umur = 18;
+$tinggi = 1.50;
+$aktif = true;
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur tahun <br>";
+echo "Tinggi: $tinggi meter <br>";
+echo "Status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+var_dump($nama);
+var_dump($umur);
+var_dump($tinggi);
+var_dump($aktif);
+?>
