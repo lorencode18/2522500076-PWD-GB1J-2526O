91a2f5f5ebeb162879093ec765b5bc6098fb67ce
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-04 09:52:36 +0700

    Latihan operator pembanding

--- pertemuan-06/latihan1-12.php
+++ pertemuan-06/latihan1-12.php
@@ -83,4 +83,25 @@ echo "Matakuliah: " . MATAKULIAH;
 ?> 
 <br><br>
 
+<h2>Latihan operator dasar</h2>
+<?php
+$a = 10;
+$b = 3;
 
+echo $a + $b . "<br>"; 
+echo $a % $b;
+?>
+
+<h2>Latihan operator pembanding</h2>
+<?php
+$a = 100;
+$b = "100";
+$c = 0;
+$d = false;
+
+echo "<h3>Perbandingan == dan ===</h3>";
+echo "\$a == \$b : "; var_dump($a == $b);
+echo "\$a === \$b : "; var_dump($a === $b);
+echo "\$c == \$d : "; var_dump($c == $d);
+echo "\$c === \$d : "; var_dump($c === $d);
+?>
