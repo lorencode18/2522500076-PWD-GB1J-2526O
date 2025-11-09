1ef2a7904788b2eb3ae1be78763b1b54d8234b02
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-09 13:33:03 +0700

    menampilkan session dari get_proses.php ke get.php

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -1,4 +1,9 @@
-
+<?php
+  session_start();
+  $sesname = $_SESSION["nama"];
+  $sesemail = $_SESSION["email"];
+  $sespesan = $_SESSION["pesan"];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -83,6 +88,11 @@
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
       </form>
+      <p>Terimakasih sudah menghubungi kami:
+        <label>Nama: <strong><?php echo $sesname; ?></strong></label>
+        <label>Email: <strong><?php echo $sesemail; ?></strong></label>
+        <label>Pesan: <strong><?php echo $sespesan; ?></strong></label>
+        </p>
     </section>
   </main>
 
