4ce72ca5da37d7d9add9702f84c0c78d13ec48cd
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-11-27 18:18:31 +0700

    mengubah kode pada baris 23-32

--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -7,26 +7,20 @@ $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
 
-$txtNim = $_POST["txtNim"];
-$txtNmLengkap = $_POST["txtNmLengkap"];
-$txtT4Lhr = $_POST["txtT4Lhr"];
-$txtTglLhr = $_POST["txtTglLhr"];
-$txtHobi = $_POST["txtHobi"];
-$txtPasangan = $_POST["txtPasangan"];
-$txtKerja = $_POST["txtKerja"];
-$txtNmOrtu = $_POST["txtNmOrtu"];
-$txtNmKakak = $_POST["txtNmKakak"];
-$txtNmAdik = $_POST["txtNmAdik"];
+$arrBiodata = [
+"nim" => $_POST["txtNim"] ?? "",
+"nama" => $_POST["txtNmLengkap"] ?? "",
+"tempat" => $_POST["txtT4Lhr"] ?? "",
+"tanggal" => $_POST["txtTglLhr"] ?? "",
+"hobi" => $_POST["txtHobi"] ?? "",
+"pasangan" => $_POST["txtPasangan"] ?? "",
+"pekerjaan" => $_POST["txtKerja"] ?? "",
+"ortu" => $_POST["txtNmOrtu"] ?? "",
+"kakak" => $_POST["txtNmKakak"] ?? "",
+"adik" => $_POST["txtNmAdik"] ?? ""
+];
 
-$_SESSION["txtNim"] = $txtNim;
-$_SESSION["txtNmLengkap"] = $txtNmLengkap;
-$_SESSION["txtT4Lhr"] = $txtT4Lhr;
-$_SESSION["txtTglLhr"] = $txtTglLhr;
-$_SESSION["txtHobi"] = $txtHobi;
-$_SESSION["txtPasangan"] = $txtPasangan;
-$_SESSION["txtKerja"] = $txtKerja;
-$_SESSION["txtNmOrtu"] = $txtNmOrtu;
-$_SESSION["txtNmKakak"] = $txtNmKakak;
-$_SESSION["txtNmAdik"] = $txtNmAdik;
+$_SESSION["biodata"] = $arrBiodata;
+header("location: index.php#about");
 header("location: index.php");
 ?>
