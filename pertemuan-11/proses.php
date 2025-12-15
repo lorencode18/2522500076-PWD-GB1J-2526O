1f776152a1b10ba09b775ddf54722ce344a1130c
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-12-15 12:23:49 +0700

    menambahkan kode untuk mengatasi error pada proses.php

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require_once __DIR__ . '/fungsi.php';
+require 'koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
   $_SESSION['flash_error'] = 'Akses tidak valid.';
