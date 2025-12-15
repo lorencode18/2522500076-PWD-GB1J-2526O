10cb2ae614cdcf8f43b125664acdd2a7b79e270f
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-12-15 11:43:53 +0700

    copy file pertemuan-10 dan paste di pertemuan-11

---
+++ pertemuan-11/localhost.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 04, 2025 at 11:13 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_pwd2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
+(1, 'Prita Lauren', '2522500076@mahasiswa.atmaluhur.ac.id', 'hallooooo'),
+(2, 'Lauren', 'lauren@gmail.com', 'semangat'),
+(3, 'Loretaaa', 'loreta@gmail.com', 'keren banget');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
