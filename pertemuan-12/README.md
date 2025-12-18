8eb511bfd88d375729a5dd6b234d750e6598cb7b
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:09:07 +0700

    copy file pertemuan-11 dan paste di pertemuan-12

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -1,1 +1,9 @@
-# pertemuan-12
+# pertemuan-11
+
+NIM : 2522500076<br>
+Nama : Prita Lauren<br>
+<br></br>
+Hari ini, Kamis 18 Desember 2025, saya belajar:<br>
+<ol>
+  <li>copy file pertemuan-11 dan paste di pertemuan-12</li>
+</ol>
---
+++ pertemuan-12/read.php
@@ -0,0 +1,28 @@
+<?php
+require 'koneksi.php';
+
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
+$no = 1;
+?>
+<table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>NO</th>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Pesan</th>
+    <th>Created At</th>
+  </tr>
+
+  <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+      <td><?= $no++; ?></td>
+      <td><?= $row['cid']; ?></td>
+      <td><?= htmlspecialchars($row['cnama']); ?></td>
+      <td><?= htmlspecialchars($row['cemail']); ?></td>
+      <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
+      <td><?= date('d-m-Y H:i', strtotime($row['dcreated_at'])); ?></td>
+    </tr>
+  <?php endwhile; ?>
+  </table>
