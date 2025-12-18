f7ad8c32905ff64be3471a16438f6544426c1c1a
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:41:31 +0700

    menambahkan penampil flash maupun error

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -7,4 +7,5 @@ Hari ini, Kamis 18 Desember 2025, saya belajar:<br>
 <ol>
   <li>copy file pertemuan-11 dan paste di pertemuan-12</li> <li>menambahkan kolom Aksi setelah kolom No dengan isi Aksi sama dengan ID</li>
   <li>membuat link edit yang aman, yaitu cid di-casting saat output (HTML)</li>
+  <li>menambahkan penampil flash maupun error</li>
 </ol>
--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,10 +1,36 @@
 <?php
 require 'koneksi.php';
+require 'fungsi.php';
 
-$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
-$q = mysqli_query($conn, $sql);
-$no = 1;
+  $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+  $q = mysqli_query($conn, $sql);
+  $no = 1;
+  if (!$q) {
+    die("Query error: ". mysqli_error($conn));
+  }
 ?>
+
+<?php
+  $flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukses
+  $flash_error  = $_SESSION['flash_error'] ?? ''; #jika ada error
+  #bersihkan session ini
+  unset($_SESSION['flash_sukses'], $_SESSION['flash_error']);
+?>
+
+<?php if (!empty($flash_sukses)): ?>
+        <div style="padding:10px; margin-bottom:10px;
+          ckground:#d4edda; color:#155724; border-radius:6px;">
+          <?= $flash_sukses; ?>
+        </div>
+<?php endif; ?>
+
+<?php if (!empty($flash_error)): ?>
+        <div style="padding:10px; margin-bottom:10px;
+          ckground:#f8d7da; color:#721c24; border-radius:6px;">
+          <?= $flash_error; ?>
+        </div>
+<?php endif; ?>
+
 <table border="1" cellpadding="8" cellspacing="0">
   <tr>
     <th>NO</th>
