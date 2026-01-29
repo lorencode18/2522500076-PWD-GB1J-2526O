b30ee140dffa5dba4de809396a9e7beb9f5e127d
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-01-29 18:10:26 +0700

    mengetik skrip pada proses update anggota untuk menyimpan perubahan menggunakan query update dan prepared statment

--- pertemuan-16/proses_update_anggota.php
+++ pertemuan-16/proses_update_anggota.php
@@ -9,86 +9,76 @@
     redirect_ke('read.php');
   }
 
-  #validasi cid wajib angka dan > 0
-  $cid = filter_input(INPUT_POST, 'cid', FILTER_VALIDATE_INT, [
+  #validasi noangg wajib angka dan > 0
+  $noangg = filter_input(INPUT_POST, 'noangg', FILTER_VALIDATE_INT, [
     'options' => ['min_range' => 1]
   ]);
 
-  if (!$cid) {
-    $_SESSION['flash_error'] = 'CID Tidak Valid.';
-    redirect_ke('edit.php?cid='. (int)$cid);
+  if (!$noangg) {
+    $_SESSION['flash_error'] = 'noangg Tidak Valid.';
+    redirect_ke('edit.php?noangg='. (int)$noangg);
   }
 
   #ambil dan bersihkan (sanitasi) nilai dari form
-  $nama  = bersihkan($_POST['txtNamaEd']  ?? '');
-  $email = bersihkan($_POST['txtEmailEd'] ?? '');
-  $pesan = bersihkan($_POST['txtPesanEd'] ?? '');
-  $captcha = bersihkan($_POST['txtCaptcha'] ?? '');
+  $noangg     = (int) ($_POST['txtNoAng']  ?? '');
+  $nmangg     = bersihkan($_POST['txtNmAng'] ?? '');
+  $jabatan    = bersihkan($_POST['txtJabAng'] ?? '');
+  $tgljadi    = bersihkan($_POST['txtTglJadi'] ?? '');
+  $kemampuan  = bersihkan($_POST['txtSkill'] ?? '');
+  $gaji       = bersihkan($_POST['txtGaji'] ?? '');
+  $nowa       = bersihkan($_POST['txtNoWA'] ?? '');
+  $batalion   = bersihkan($_POST['txtBatalion'] ?? '');
+  $bb         = bersihkan($_POST['txtBB'] ?? '');
+  $tb         = bersihkan($_POST['txtTB'] ?? '');
 
-  #Validasi sederhana
   $errors = []; #ini array untuk menampung semua error yang ada
 
-  if ($nama === '') {
-    $errors[] = 'Nama wajib diisi.';
-  }
+if ($noangg <= 0) {
+  $errors[] = 'Nomor Anggota wajib diisi.';
+}
 
-  if ($email === '') {
-    $errors[] = 'Email wajib diisi.';
-  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = 'Format e-mail tidak valid.';
-  }
+if ($nama_anggota === '') {
+  $errors[] = 'Nama Anggota wajib diisi.';
+}
 
-  if ($pesan === '') {
-    $errors[] = 'Pesan wajib diisi.';
-  }
+if (mb_strlen($nama_anggota) < 3) {
+  $errors[] = 'Nama Anggota minimal 3 karakter.';
+}
 
-  if ($captcha === '') {
-    $errors[] = 'Pertanyaan wajib diisi.';
-  }
-
-  if (mb_strlen($nama) < 3) {
-    $errors[] = 'Nama minimal 3 karakter.';
-  }
-
-  if (mb_strlen($pesan) < 10) {
-    $errors[] = 'Pesan minimal 10 karakter.';
-  }
-
-  if ($captcha!=="6") {
-    $errors[] = 'Jawaban '. $captcha.' captcha salah.';
-  }
-
-  /*
-  kondisi di bawah ini hanya dikerjakan jika ada error, 
-  simpan nilai lama dan pesan error, lalu redirect (konsep PRG)
-  */
-  if (!empty($errors)) {
-    $_SESSION['old'] = [
-      'nama'  => $nama,
-      'email' => $email,
-      'pesan' => $pesan
-    ];
+if (!empty($errors)) {
+  $_SESSION['old'] = [
+    'noangg'    => $noangg,
+    'nmangg'    => $nmangg,
+    'jabatan'   => $jabatan,
+    'tgljadi'   => $tgljadi,
+    'kemampuan' => $kemampuan,
+    'gaji'      => $gaji,
+    'nowa'      => $nowa,
+    'batalion'  => $batalion,
+    'bb'        => $bb,
+    'tb'        => $tb,
+  ];
 
     $_SESSION['flash_error'] = implode('<br>', $errors);
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('edit_anggota.php?noangg='. (int)$noangg);
   }
 
   /*
     Prepared statement untuk anti SQL injection.
     menyiapkan query UPDATE dengan prepared statement 
-    (WAJIB WHERE cid = ?)
+    (WAJIB WHERE noangg = ?)
   */
-  $stmt = mysqli_prepare($conn, "UPDATE tbl_tamu 
-                                SET cnama = ?, cemail = ?, cpesan = ? 
-                                WHERE cid = ?");
+  $stmt = mysqli_prepare($conn, "UPDATE tbl_anggota
+                                SET Nama_Anggota = ?,  Jabatan = ?, Tanggal_Jadi = ?, Kemampuan = ?, Gaji = ?, Nomor_WA = ?, Batalion = ?, Berat_Badan = ?, Tinggi_Badan = ?
+                                WHERE Nomor_Anggota = ?");
   if (!$stmt) {
     #jika gagal prepare, kirim pesan error (tanpa detail sensitif)
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('edit.php?noangg='. (int)$noangg);
   }
 
   #bind parameter dan eksekusi (s = string, i = integer)
-  mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $cid);
+  mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $noangg);
 
   if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value
     unset($_SESSION['old']);
@@ -104,9 +94,9 @@
       'pesan' => $pesan,
     ];
     $_SESSION['flash_error'] = 'Data gagal diperbaharui. Silakan coba lagi.';
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('edit.php?noangg='. (int)$noangg);
   }
   #tutup statement
   mysqli_stmt_close($stmt);
 
-  redirect_ke('edit.php?cid='. (int)$cid);
+  redirect_ke('edit.php?noangg='. (int)$noangg);
